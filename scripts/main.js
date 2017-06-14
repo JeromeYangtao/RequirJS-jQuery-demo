4f702cf2c5f7d2fadbb020274532170d9faa6753
Thomson <779387850@qq.com>
2017-06-14 15:21:39 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+requirejs.config({
+    baseUrl: 'scripts/helper',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery-3.2.1.min',
+        util: 'util'
+    }
+});
+
+
+
+
+require(['jquery', 'util'], function($) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+    $('body').text('jQuery hello world')
+});
